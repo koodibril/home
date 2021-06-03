8b25ea80831c88fd7f9572cb42707508dade7cb4
AlphaBril <florian.marie.doucet@gmail.com>
2021-06-03 11:34:25 +0200

    added babylon js to project

---
+++ src/main/webapp/app/babylon/babylon.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BabylonComponent } from './babylon.component';
+
+describe('BabylonComponent', () => {
+  let component: BabylonComponent;
+  let fixture: ComponentFixture<BabylonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BabylonComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BabylonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/main/webapp/app/babylon/babylon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-babylon',
+  templateUrl: './babylon.component.html',
+  styleUrls: ['./babylon.component.scss'],
+})
+export class BabylonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
